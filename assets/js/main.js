c58d9a28bb52f62930fd11789c350e8b83ca67ee
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-11-12 14:57:14 +0100

    pwdgen 1.0
    Generator done and working

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+var name = prompt("What's your name?");
+var surname = prompt("What's your surname?");
+var favcolor = prompt("What's your favorite color?");
+document.getElementById('generatedPassword').innerHTML = name + surname + favcolor + "19";
